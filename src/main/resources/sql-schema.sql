d391201ee051e0bf2cd12c952bd851daec22fc40
Knoah <cotto.knoah@gmail.com>
2021-06-03 17:22:43 -0500

    KC: added diagrams

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,35 +1,51 @@
-drop schema ims;
+SHOW DATABASES;
+# SELECT database();
 
+DROP SCHEMA ims;
 CREATE SCHEMA IF NOT EXISTS `ims`;
-
 USE `ims` ;
 
+# SHOW TABLES;
+
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-     PRIMARY KEY (`id`)
+    `last_name` VARCHAR(40) DEFAULT NULL,
+    `phone_number` INT(11) UNSIGNED NOT NULL,
+    `email` VARCHAR(40) DEFAULT NULL,
+    `password` VARCHAR(40) DEFAULT NULL,
+     PRIMARY KEY (`customer_id`),
+     UNIQUE INDEX `email_UNIQUE` (`email`)
 );
+
+DESC customers;
 
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
-       `id` INT(11) NOT NULL AUTO_INCREMENT,
-        `first_name` VARCHAR(40) DEFAULT NULL,
-        `surname` VARCHAR(40) DEFAULT NULL,
-        PRIMARY KEY (`id`)
+        `order_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `customer_id` INT(11) UNSIGNED DEFAULT NULL,
+        `order_filled` BOOLEAN DEFAULT NULL,
+        `order_date` DATETIME DEFAULT NULL,
+        `order_cost` FLOAT UNSIGNED NOT NULL,
+        PRIMARY KEY (`order_id`),
+        FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS `ims`.`items` (
-        `id` INT(11) NOT NULL AUTO_INCREMENT,
-        `first_name` VARCHAR(40) DEFAULT NULL,
-        `surname` VARCHAR(40) DEFAULT NULL,
-         PRIMARY KEY (`id`)
+# DESC orders;
+
+CREATE TABLE IF NOT EXISTS `ims`.`products` (
+        `product_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `product_name` VARCHAR(40) DEFAULT NULL,
+        `product_price` FLOAT UNSIGNED DEFAULT NULL,
+         PRIMARY KEY (`product_id`)
 );
+
+# DESC products;
 
 # Table the connects Many to Many Relationships
-CREATE TABLE IF NOT EXISTS `ims`.`orders_items` (
-         `id` INT(11) NOT NULL AUTO_INCREMENT,
-         `first_name` VARCHAR(40) DEFAULT NULL,
-         `surname` VARCHAR(40) DEFAULT NULL,
-         PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `ims`.`orders_products` (
+         `orders_order_id` INT(11) UNSIGNED NULL,
+         `products_product_id` INT(11) UNSIGNED NOT NULL,
+         FOREIGN KEY (orders_order_id) REFERENCES orders (order_id),
+         FOREIGN KEY (products_product_id) REFERENCES products (product_id)
 );
 
