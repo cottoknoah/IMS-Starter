0e00f1380a478fcf531cc6a2878dd89945536f99
Knoah <cotto.knoah@gmail.com>
2021-06-03 13:12:41 -0500

    KC: Additional test classes added

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+import com.qa.ims.persistence.dao.Dao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -8,29 +8,28 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
-                                                 `id` INT(11) NOT NULL AUTO_INCREMENT,
-                                                 `first_name` VARCHAR(40) DEFAULT NULL,
-                                                 `surname` VARCHAR(40) DEFAULT NULL,
-                                                 PRIMARY KEY (`id`)
+       `id` INT(11) NOT NULL AUTO_INCREMENT,
+        `first_name` VARCHAR(40) DEFAULT NULL,
+        `surname` VARCHAR(40) DEFAULT NULL,
+        PRIMARY KEY (`id`)
 );
 
-
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
-                                                 `id` INT(11) NOT NULL AUTO_INCREMENT,
-                                                 `first_name` VARCHAR(40) DEFAULT NULL,
-                                                 `surname` VARCHAR(40) DEFAULT NULL,
-                                                 PRIMARY KEY (`id`)
+        `id` INT(11) NOT NULL AUTO_INCREMENT,
+        `first_name` VARCHAR(40) DEFAULT NULL,
+        `surname` VARCHAR(40) DEFAULT NULL,
+         PRIMARY KEY (`id`)
 );
 
 # Table the connects Many to Many Relationships
 CREATE TABLE IF NOT EXISTS `ims`.`orders_items` (
-                                                 `id` INT(11) NOT NULL AUTO_INCREMENT,
-                                                 `first_name` VARCHAR(40) DEFAULT NULL,
-                                                 `surname` VARCHAR(40) DEFAULT NULL,
-                                                 PRIMARY KEY (`id`)
+         `id` INT(11) NOT NULL AUTO_INCREMENT,
+         `first_name` VARCHAR(40) DEFAULT NULL,
+         `surname` VARCHAR(40) DEFAULT NULL,
+         PRIMARY KEY (`id`)
 );
 
